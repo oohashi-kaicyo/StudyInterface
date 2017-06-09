0115af09daddc70ef0c69bed3a4968d7f419cc00
yasuhiro hashimoto <0084kaicyo@gmail.com>
2017-06-09 23:27:53 +0900

    ファイル名を変更

---
+++ BattleCharacter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StudyInterface
+{
+    public abstract class BattleCharacter
+    {
+        public int      Hp             { get; set; }
+        public IWeapon  Weapon         { get; set; }
+		public string[] Characteristic { get; set; }
+		public void Attack(ref BattleCharacter enemy) {
+			BattleCharacter myself = this;
+			Damage damage = new Damage(ref myself, ref enemy);
+		}
+	}
+}
